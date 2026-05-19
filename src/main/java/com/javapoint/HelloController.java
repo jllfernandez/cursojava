6178388a1bc3864e3eac1d2ea617e4808ce5d413
jllfernandez@yahoo.com <jllfernandez@yahoo.com>
2026-05-19 13:22:50 +0200

    mandandolo

--- src/main/java/com/javapoint/HelloController.java
+++ src/main/java/com/javapoint/HelloController.java
@@ -3,7 +3,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;  
 @Controller  
 /*
- * Comentario
+ * Comentario 2
  */
 public class HelloController {  
 @RequestMapping("/")  
